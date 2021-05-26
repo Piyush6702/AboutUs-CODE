e736d0d80eb487607ecb328634807373552eeb9c
gautism1 <vipingautam.m@gmail.com>
2021-05-26 19:56:10 +0530

    removed padding in social div

--- src/components/social.js
+++ src/components/social.js
@@ -6,17 +6,17 @@ function Social() {
 
         <ul>
               <li>
-                  <a href="https://www.youtube.com/channel/UCgxZ9rqaWscw-kOfkj2F2lQ" target="_blank" rel="noreferrer" >Youtube
+                  <a href="https://www.youtube.com/channel/UCgxZ9rqaWscw-kOfkj2F2lQ" target="_blank" rel="noreferrer" >Ytube
                   </a>
               </li>
               <li>
-                  <a href="https://www.facebook.com/codebietjhs/" target="_blank" rel="noreferrer" >Facebook</a>
+                  <a href="https://www.facebook.com/codebietjhs/" target="_blank" rel="noreferrer" >Fb</a>
               </li>
               <li>
-                  <a href="https://www.linkedin.com/company/codebiet/" target="_blank" rel="noreferrer" >Linkedin</a>
+                  <a href="https://www.linkedin.com/company/codebiet/" target="_blank" rel="noreferrer" >Ldin</a>
               </li>
               <li>
-                  <a href="https://t.me/joinchat/AAAAAFMLj2wLbN8JYCMtlg" target="_blank" rel="noreferrer" >Telegram</a>
+                  <a href="https://t.me/joinchat/AAAAAFMLj2wLbN8JYCMtlg" target="_blank" rel="noreferrer" >Tgram</a>
               </li>
           </ul>
         </div>
